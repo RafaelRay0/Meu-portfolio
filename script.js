672daa54ec64d7699ce8e1c68468d8d7635c0456
RafaelRay0 <rr1010ray@gmail.com>
2024-02-01 13:00:11 -0300

    Atualizado

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function abrirMenu() {
+const menu = document.getElementById('menu-mobile')
+
+    menu.classList.add('abrir-menu')
+}
+
+function feixaMenu() {
+const menu = document.getElementById('menu-mobile')
+
+    menu.classList.remove('abrir-menu')    
+}
+
